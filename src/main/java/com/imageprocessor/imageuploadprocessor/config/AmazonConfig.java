b6ff4f4eb9fe105909ef5d8469de86dbd5a12ae6
Aakash Deogaonkar <deogaonkar.a@husky.neu.edu>
2020-12-22 00:51:39 -0500

    Update AmazonConfig.java

--- src/main/java/com/imageprocessor/imageuploadprocessor/config/AmazonConfig.java
+++ src/main/java/com/imageprocessor/imageuploadprocessor/config/AmazonConfig.java
@@ -14,7 +14,7 @@ public class AmazonConfig {
 
 	@Bean
 	public AmazonS3 s3() {
-		AWSCredentials awsCredentials = new BasicAWSCredentials("", "");
+		AWSCredentials awsCredentials = new BasicAWSCredentials("","");
 		
 		return AmazonS3ClientBuilder
 				.standard()
